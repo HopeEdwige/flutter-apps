ef4b1a79aa524a3db20fbdf8abfea5a901770c26
Mohammad Wali <mohdwali@live.com>
2020-12-26 18:50:58 +0100

    Preventing back button while quiz is in progress.

--- quiz_app/lib/screens/quiz_screen/quiz_screen.dart
+++ quiz_app/lib/screens/quiz_screen/quiz_screen.dart
@@ -37,99 +37,102 @@ class QuizScreenState extends State<QuizScreen> {
     final topicColor = widget.topic.iconColor;
     final Question currentQuestion = widget.questions[_currentQuestionIndex];
 
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Container(
-        width: size.width,
-        height: size.height,
-        child: CustomPaint(
-          painter: Pattern(
-            canvasColor: theme.canvasColor,
-            color: topicColor,
-          ),
-          child: Container(
-            width: size.width,
-            height: size.height,
-            margin: const EdgeInsets.symmetric(vertical: 4),
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Column(
-              children: [
-                Text(
-                  widget.topic.name,
-                  style: const TextStyle(fontSize: 25, color: Colors.white),
-                ),
-                Container(
-                  height: 4,
-                  width: size.width / 6,
-                  color: topicColor,
-                  margin: const EdgeInsets.only(top: 8),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                DotIndicator(
-                  position: _currentQuestionIndex,
-                  dotsCount: widget.questions.length,
-                  lineColor: Colors.grey,
-                  dotColor: Colors.grey[600],
-                  completedDotColor: topicColor,
-                  completedLineColor: topicColor[700],
-                  activeDotColor: Colors.transparent,
-                  activeBorderColor: topicColor[400],
-                  finishDotColor: Colors.yellow[700],
-                ),
-                Expanded(
-                  child: Column(
-                    children: [
-                      SizedBox(height: size.height / 30),
-                      Text(
-                        currentQuestion.question,
-                        style: theme.textTheme.headline5,
-                      ),
-                      SizedBox(height: size.height / 30),
-                      ListView.separated(
-                        shrinkWrap: true,
-                        itemBuilder: (BuildContext context, int index) {
-                          final isSelected = index == _currentSelectedAnswerIndex;
-                          return ListTile(
-                            selected: isSelected,
-                            tileColor: theme.cardColor,
-                            selectedTileColor: Colors.yellow[700],
-                            onTap: () => _handleAnswerSelection(index),
-                            title: Text(currentQuestion.answers[index], style: theme.textTheme.subtitle1),
-                            leading: isSelected
-                                ? Icon(Icons.check, color: theme.textTheme.subtitle1.color)
-                                : Icon(Icons.radio_button_unchecked, color: theme.textTheme.subtitle1.color),
-                          );
-                        },
-                        itemCount: currentQuestion.answers.length,
-                        separatorBuilder: (BuildContext context, int index) => SizedBox(height: size.height / 40),
-                      ),
-                      MaterialButton(
-                        minWidth: double.maxFinite,
-                        disabledColor: Color(topicColor[700].hashCode).withAlpha(90),
-                        color: topicColor[700],
-                        onPressed: _currentSelectedAnswerIndex == -1 ? null : () => _handleNextClick(),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Next",
-                              style: const TextStyle(fontSize: 20),
-                            ),
-                            SizedBox(width: 10),
-                            Icon(Icons.arrow_forward),
-                          ],
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 14),
-                      )
-                    ],
+    return WillPopScope(
+      onWillPop: _handleOnWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Container(
+          width: size.width,
+          height: size.height,
+          child: CustomPaint(
+            painter: Pattern(
+              canvasColor: theme.canvasColor,
+              color: topicColor,
+            ),
+            child: Container(
+              width: size.width,
+              height: size.height,
+              margin: const EdgeInsets.symmetric(vertical: 4),
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                children: [
+                  Text(
+                    widget.topic.name,
+                    style: const TextStyle(fontSize: 25, color: Colors.white),
                   ),
-                )
-              ],
+                  Container(
+                    height: 4,
+                    width: size.width / 6,
+                    color: topicColor,
+                    margin: const EdgeInsets.only(top: 8),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  DotIndicator(
+                    position: _currentQuestionIndex,
+                    dotsCount: widget.questions.length,
+                    lineColor: Colors.grey,
+                    dotColor: Colors.grey[600],
+                    completedDotColor: topicColor,
+                    completedLineColor: topicColor[700],
+                    activeDotColor: Colors.transparent,
+                    activeBorderColor: topicColor[400],
+                    finishDotColor: Colors.yellow[700],
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        SizedBox(height: size.height / 30),
+                        Text(
+                          currentQuestion.question,
+                          style: theme.textTheme.headline5,
+                        ),
+                        SizedBox(height: size.height / 30),
+                        ListView.separated(
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            final isSelected = index == _currentSelectedAnswerIndex;
+                            return ListTile(
+                              selected: isSelected,
+                              tileColor: theme.cardColor,
+                              selectedTileColor: Colors.yellow[700],
+                              onTap: () => _handleAnswerSelection(index),
+                              title: Text(currentQuestion.answers[index], style: theme.textTheme.subtitle1),
+                              leading: isSelected
+                                  ? Icon(Icons.check, color: theme.textTheme.subtitle1.color)
+                                  : Icon(Icons.radio_button_unchecked, color: theme.textTheme.subtitle1.color),
+                            );
+                          },
+                          itemCount: currentQuestion.answers.length,
+                          separatorBuilder: (BuildContext context, int index) => SizedBox(height: size.height / 40),
+                        ),
+                        MaterialButton(
+                          minWidth: double.maxFinite,
+                          disabledColor: Color(topicColor[700].hashCode).withAlpha(90),
+                          color: topicColor[700],
+                          onPressed: _currentSelectedAnswerIndex == -1 ? null : () => _handleNextClick(),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Next",
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                              SizedBox(width: 10),
+                              Icon(Icons.arrow_forward),
+                            ],
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 14),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -153,6 +156,32 @@ class QuizScreenState extends State<QuizScreen> {
     } else {
       print(_selectedAnswerIndexes);
     }
+  }
+
+  Future<bool> _handleOnWillPop() async {
+    return showDialog<bool>(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          content: Text("Are you sure you want to quit the quiz? All your progress will be lost."),
+          title: Text("Warning!"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("Yes"),
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+            ),
+            FlatButton(
+              child: Text("No"),
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
 
