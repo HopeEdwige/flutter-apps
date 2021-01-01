09609c3cf9dec21e316219ffa176c3db38bc4da0
Mohammad Wali <mohdwali@live.com>
2021-01-01 09:40:38 +0100

    Fixing alignment for not found screen

--- whatsapp_status_saver/lib/screens/not_found_screen/not_found_screen.dart
+++ whatsapp_status_saver/lib/screens/not_found_screen/not_found_screen.dart
@@ -5,57 +5,62 @@ class NotFoundScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
+    final Size size = MediaQuery.of(context).size;
 
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16, top: 8),
-                child: Image.asset(
-                  'assets/images/error.png',
-                  scale: 2,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16, top: 8),
-                child: Text(
-                  "Oh Noo!",
-                  style: theme.textTheme.headline3,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16),
-                child: Text(
-                  "Looks like you haven't installed WhatsApp yet.",
-                  style: theme.textTheme.subtitle1.copyWith(
-                    fontSize: 20,
-                    height: 1.5,
+        child: Container(
+          width: size.width,
+          height: size.height,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16, top: 8),
+                  child: Image.asset(
+                    'assets/images/error.png',
+                    scale: 2,
                   ),
-                  textAlign: TextAlign.center,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: ElevatedButton(
-                  onPressed: () {
-                    StoreRedirect.redirect(androidAppId: "com.whatsapp", iOSAppId: "");
-                  },
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16, top: 8),
                   child: Text(
-                    'Install Now',
-                    style: TextStyle(fontSize: 20),
+                    "Oh Noo!",
+                    style: theme.textTheme.headline3,
                   ),
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(
-                      const EdgeInsets.symmetric(vertical: 14, horizontal: 32),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Text(
+                    "Looks like you haven't installed WhatsApp yet.",
+                    style: theme.textTheme.subtitle1.copyWith(
+                      fontSize: 20,
+                      height: 1.5,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      StoreRedirect.redirect(androidAppId: "com.whatsapp", iOSAppId: "");
+                    },
+                    child: Text(
+                      'Install Now',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(vertical: 14, horizontal: 32),
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
